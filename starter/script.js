f4ca14785276c466c0cdd087d127dd5d7889b0b6
Sparrow <dfleites1234@gmail.com>
2022-08-02 14:15:41 -0500

    Working With Classes
    Made code simpler and used click function as well as removing classes.

--- starter/script.js
+++ starter/script.js
@@ -4,7 +4,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const OpenModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', OpenModal);
+
+// The . is only for the selector. EX: const modal = document.querySelector('.modal');
+
+// The . is not used when calling a function
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
