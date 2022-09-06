e8dce1f32be8eeed06e9f0a8374db812713f9e6d
Sparrow <dfleites1234@gmail.com>
2022-09-06 18:28:15 -0500

    Handling an "Esc" Keypress Event
    Use the document.addEventListener('keydown', function (e)){
    console.log(e.key);
    }

--- starter/script.js
+++ starter/script.js
@@ -25,3 +25,9 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape') 
+});
